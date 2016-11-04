7aa6767810bb87cef76b88a2be5fd3170a00b2bf
zhanglinjie412@gmail.com <zhanglinjie412@gmail.com>
2016-11-04 09:37:38 +0800

    error test

--- spec/radar_spec.rb
+++ spec/radar_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Radar, ".status_for" do
     end
 
     it "has a IATA code" do
-      expect(status.code).to eq("JFK")
+      expect(status.code).to eq("JFK error")
     end
 
     it "has a name" do
